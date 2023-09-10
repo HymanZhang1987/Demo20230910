393c0ba9d2de10e3aac9344a912022f4fef9343c
HZhang21 <hzhang21@rockwellautomation.com>
2023-09-10 20:36:46 +0800

    this is new project for test

--- pythonProject1958/main.py
+++ pythonProject1958/main.py
@@ -12,5 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print('this is a new test for 6.40.00.00')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

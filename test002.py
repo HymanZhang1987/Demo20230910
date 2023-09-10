40700e59e826eabeaa40e5f218c58d890ad0f8f9
HZhang21 <hzhang21@rockwellautomation.com>
2023-09-10 17:18:05 +0800

    this is a test from client side.

--- test002.py
+++ test002.py
@@ -1,4 +1,6 @@
-import timer
+import time
 print('this is a test0001')
 print('this is a test0002')
 print('this is a test003')
+print('this is a new test0004 from local client')
+print('I want to test this case.')
